1d9370c416d58ed495775604c53941062b10f11a
Eljana Palodhi <elianapalodi3@gmail.com>
2023-03-05 20:25:45 +0100

    added button css

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,19 @@
 import React from "react";
+import "./Button.css";
+
 export default function Button() {
   return <button> Dark Theme </button>;
+
+  function changeTheme() {
+    let body = document.querySelector("body");
+
+    if (body.classList.contains("black")) {
+      body.classList.remove("black");
+    } else {
+      body.classList.add("black");
+    }
+  }
+
+  let themeButton = document.querySelector("button");
+  themeButton.addEventListener("click, ChangeTheme");
 }
